cfbe1560b99b610a097ef70e6d8d57bd1e643258
julien gourmet <julien.gourmet@epita.fr>
2025-08-21 15:20:05 +0200

    feat(Contact): structure and box logic

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,30 @@
-export default function About() {
+import {useTranslation} from "react-i18next";
+
+export default function Contact() {
+
+    const {t} = useTranslation();
     return (
-        <div className="p-8">
-            <h1 className="text-3xl font-bold">Contact</h1>
-        </div>
+        <main className="min-h-screen px-4 pt-32 pb-16 bg-slate-900 text-slate-100">
+            <section className="container mx-auto px-4 py-24">
+                <header className="text-center mb-10">
+                    <h1 className="text-4xl md-text-5xl font-bold bg-clip-text mb-4">{t("contact.title")}</h1>
+                    <p className="text-slate-400 mt-2 text-lg md:text-xl max-w-2xl mx-auto">
+                        {t("contact.subtitle")}
+                    </p>
+                </header>
+            </section>
+            <section className="container mx-auto px-4 py-24">
+                <div className="grid gap-8 md:grid-cols-2">
+                    <div className="rounded-2xl border border-white/10 bg-white/5 p-6">
+                        box 1
+                    </div>
+                    <div className="rounded-2xl border border-white/10 bg-white/5 p-6">
+                        box 2
+                    </div>
+                </div>
+            </section>
+
+        </main>
+
     );
 }

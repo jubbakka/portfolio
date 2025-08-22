0f130c8b632ea0c6857b2cda985a8ceac931b5ed
julien gourmet <julien.gourmet@epita.fr>
2025-08-22 23:16:35 +0200

    feat (front) : remove emailjs import

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3001", // ton backend
+        target: process.env.VITE_API_URL, // récupère la valeur
         changeOrigin: true,
       },
     },
   },
-})
+});
